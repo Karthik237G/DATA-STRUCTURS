5328ec6d3bd5cc554873e640280808776ccce2b3
Karthik237G <gollapallikarthik03@gmail.com>
2024-10-17 20:30:55 +0530

    Update data types.py

--- data types.py
+++ data types.py
@@ -17,3 +17,14 @@ print(list)
 list.append(list2) #output [1,2,'ram',2.7,['ram','lax']]
 #extend of a single element of string data will act as a split
 list.extend('ram') #output [list,'r','a','m']
+
+
+#tuple()
+tuple=(1,2,3,4,8,9,45)
+tuple2=(1,5,6)
+print(tuple+tuple2)
+#we cannot directly delete or insert but we can use slicing and striding techniques and alter them by creating a new tuple 
+new_tup=tuple[:1]+tuple[2:] #output (1, 3, 4, 8, 9, 45) 2 is deleted using slicing
+
+
+
